168f53233e754fe20fa05435008f22c8ec86602a
Michael Perry <lmperry@stanford.edu>
2019-08-16 16:29:16 -0700

    assign_type for PatientWeight

--- run.py
+++ run.py
@@ -155,7 +155,7 @@ def get_sex_string(sex_str):
     return sex
 
 
-def assign_type(s):
+def (s):
     """
     Sets the type of a given input.
     """
@@ -544,7 +544,7 @@ def dicom_to_json(zip_file_path, outbase, timezone):
     if session_label:
         metadata['session']['label'] = session_label
     if hasattr(dcm, 'PatientWeight') and dcm.get('PatientWeight'):
-        metadata['session']['weight'] = dcm.get('PatientWeight')
+        metadata['session']['weight'] = assign_type(dcm.get('PatientWeight'))
 
     # Subject Metadata
     metadata['session']['subject'] = {}
