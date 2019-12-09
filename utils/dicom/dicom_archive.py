ae193ec100c4dbb4a2a249480c60818b13e17233
Kaleb Fischer <kalebfischer@flywheel.io>
2019-12-09 09:06:59 -0600

    add functionality for splitting embedded localizers

---
+++ utils/dicom/dicom_archive.py
@@ -0,0 +1,203 @@
+import contextlib
+import logging
+import os
+import re
+import shutil
+import tempfile
+import zipfile
+
+import pydicom
+
+from .dicom_metadata import get_pydicom_header
+
+log = logging.getLogger(__name__)
+
+
+@contextlib.contextmanager
+def make_temp_directory():
+    temp_dir = tempfile.mkdtemp()
+    try:
+        yield temp_dir
+    finally:
+        shutil.rmtree(temp_dir)
+
+
+def create_zip_from_file_list(root_dir, file_list, output_path, comment=None):
+    with zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for fp in file_list:
+            zipf.write(fp, os.path.relpath(fp, root_dir))
+    return output_path
+
+
+def append_str_to_dcm_zip_path(dcm_zip_path, append_str):
+    if re.match(r'(.*)((\.dicom\.zip)|(\.dcm\.zip))', dcm_zip_path):
+        out_path = re.sub(r'(.*)((\.dicom\.zip)|(\.dcm\.zip))', f'\\1{append_str}\\2', dcm_zip_path)
+    elif re.match(r'(.*)(\.zip)', dcm_zip_path):
+        out_path = re.sub(r'(.*)(\.zip)', f'\\1{append_str}\\2', dcm_zip_path)
+    else:
+        out_path = dcm_zip_path + append_str
+        log.warning(
+            'Did not recognize a standard extension for DICOM '
+            f'archive for {os.path.basename(dcm_zip_path)}. '
+            f'using {out_path}'
+        )
+    return out_path
+
+
+def extract_files(zip_path, output_directory):
+    """
+    extracts the files in a zip to an output directory
+    :param zip_path: path to the zip to extract
+    :param output_directory: directory to which to extract the files
+    :return: file_list, a list to the paths of the extracted files and comment, the archive comment
+    """
+    with zipfile.ZipFile(zip_path, 'r') as zipf:
+        zipf.extractall(output_directory)
+        file_list = zipf.namelist()
+        # Get full paths and remove directories from list
+        file_list = [os.path.join(output_directory, fp) for fp in file_list if not fp.endswith(os.path.sep)]
+
+    return file_list
+
+
+class DicomFile:
+    def __init__(self, file_path, root_path, force=False):
+        self.path = file_path
+        self.relpath = os.path.relpath(file_path, root_path)
+
+        try:
+            self.dataset = pydicom.dcmread(file_path, force=force)
+            self.header_dict = get_pydicom_header(self.dataset)
+        except Exception as e:
+            filename = os.path.basename(file_path)
+            log.error(f'Exception occurred when reading {filename}: {e}')
+
+
+class DicomArchive:
+    def __init__(self, zip_path, extract_dir, dataset_list=False):
+        self.path = zip_path
+        self.dataset = None
+        self.dataset_list = None
+        self.extract_dir = extract_dir
+        with zipfile.ZipFile(self.path) as zipf:
+            file_list = zipf.namelist()
+            # Get full paths and remove directories from list
+            self.file_list = [fp for fp in file_list if not fp.endswith(os.path.sep)]
+        try:
+            self.initialize_dataset(dataset_list=dataset_list)
+        except Exception as e:
+            log.error(f'An exception occurred while parsing {zip_path}: {e}')
+
+    def initialize_dataset(self, dataset_list=False):
+        if dataset_list:
+            self.dataset_list = list()
+        for fp in self.file_list:
+            with zipfile.ZipFile(self.path) as zipf:
+                extract_path = zipf.extract(fp, self.extract_dir)
+                if os.path.isfile(extract_path):
+                    dicom_file = DicomFile(extract_path, self.extract_dir)
+                    file_dataset = dicom_file.dataset
+                    if file_dataset:
+                        # Here we check for the Raw Data Storage SOP Class, if there
+                        # are other pydicom files in the zip then we read the next one,
+                        # if this is the only class of pydicom in the file, we accept
+                        # our fate and move on.
+                        if file_dataset.get('SOPClassUID') == 'Raw Data Storage' and not self.dataset:
+                            log.info(f'{os.path.basename(fp)} is Raw Data Storage. Skipping...')
+                            continue
+                        if dataset_list:
+                            self.dataset_list.append(dicom_file)
+                            if not self.dataset:
+                                self.dataset = file_dataset
+                        else:
+                            self.dataset = file_dataset
+                            break
+
+    def dicom_tag_value_list(self, dicom_tag):
+        if not self.dataset_list:
+            self.initialize_dataset(self.extract_dir)
+
+        if not self.dataset.get(dicom_tag):
+            log.error(f'{dicom_tag} is missing from {os.path.basename(self.path)}')
+            return None
+        else:
+            return [dicom_file.dataset.get(dicom_tag) for dicom_file in self.dataset_list if dicom_file.dataset]
+
+    def dicom_tag_value_dict(self, dicom_tag):
+        if not self.dataset_list:
+            self.initialize_dataset(self.extract_dir)
+
+        if not self.dataset.get(dicom_tag):
+            log.error(f'{dicom_tag} is missing from {os.path.basename(self.path)}')
+            return None
+        else:
+            value_dict = dict()
+            for dicom_file in self.dataset_list:
+                tag_value = dicom_file.header_dict.get(dicom_tag)
+                if tag_value:
+                    if type(tag_value) == list:
+                        tag_value_key = tuple(tag_value)
+                    else:
+                        tag_value_key = tag_value
+                else:
+                    tag_value_key = 'NA'
+                if not value_dict.get(tag_value_key):
+                    value_dict[tag_value_key] = list()
+                value_dict[tag_value_key].append(dicom_file.path)
+            return value_dict
+
+    def split_archive_on_unique_tag(self, dicom_tag, output_dir, append_str, all_unique=True):
+        tag_dict = self.dicom_tag_value_dict(dicom_tag)
+        top_value = max(tag_dict, key=lambda x: len(tag_dict[x]))
+
+        index = 1
+        for tag_value, image_paths in tag_dict.items():
+            if tag_value == top_value:
+                out_path = os.path.join(output_dir, os.path.basename(self.path))
+                create_zip_from_file_list(self.extract_dir, image_paths, out_path)
+                if not all_unique:
+                    out_path = append_str_to_dcm_zip_path(out_path, append_str)
+                    other_image_paths = [dcm.path for dcm in self.dataset_list if dcm.path not in image_paths]
+                    create_zip_from_file_list(self.extract_dir, other_image_paths, out_path)
+            elif len(tag_dict.keys()) > 2 and all_unique:
+
+                tmp_append_str = append_str + str(index)
+                index += 1
+                basename = append_str_to_dcm_zip_path(os.path.basename(self.path), tmp_append_str)
+                out_path = os.path.join(output_dir, basename)
+                create_zip_from_file_list(self.extract_dir, image_paths, out_path)
+            else:
+                continue
+
+    def contains_embedded_localizer(self):
+        embedded_localizer = False
+        iop_value_list = self.dicom_tag_value_list('ImageOrientationPatient')
+        # Convert to list of tuples so it's hashable for set
+        iop_tuple_list = [tuple(iop_val) for iop_val in iop_value_list]
+        image_count = len(iop_tuple_list)
+        iop_tuple_set = set(iop_tuple_list)
+        nunique_iop = len(iop_tuple_set)
+        # If there's more than one unique IOP, it's a localizer
+        if nunique_iop > 1:
+            # Only a scan series with embedded localizer if the number of unique IOP
+            # values/ total images is less than 0.20
+            if (nunique_iop / image_count) < 0.20:
+                embedded_localizer = True
+        return embedded_localizer
+
+    def select_files_by_tag_value(self, dicom_tag, value):
+        if not self.dataset_list:
+            self.initialize_dataset(self.extract_dir)
+        if not self.dataset.get(dicom_tag):
+            log.error(f'{dicom_tag} is missing from {os.path.basename(self.path)}')
+
+        else:
+            match_list = list()
+            not_match_list = list()
+            for dicom_file in self.dataset_list:
+                if dicom_file.header_dict.get(dicom_tag) == value:
+
+                    match_list.append(dicom_file)
+                else:
+                    not_match_list.append(dicom_file)
+            return match_list, not_match_list
---
+++ utils/dicom/dicom_metadata.py
@@ -0,0 +1,346 @@
+import datetime
+import logging
+import re
+import string
+
+import nibabel
+import pytz
+import tzlocal
+import pydicom
+
+log = logging.getLogger(__name__)
+
+
+def assign_type(s):
+    """
+    Sets the type of a given input.
+    """
+    if type(s) == pydicom.valuerep.PersonName or type(s) == pydicom.valuerep.PersonName3 or type(s) == pydicom.valuerep.PersonNameBase:
+        return format_string(s)
+    if type(s) == list or type(s) == pydicom.multival.MultiValue:
+        try:
+            return [ float(x) for x in s ]
+        except ValueError:
+            try:
+                return [ int(x) for x in s ]
+            except ValueError:
+                return [ format_string(x) for x in s if len(x) > 0 ]
+    elif type(s) == float or type(s) == int:
+        return s
+    else:
+        s = str(s)
+        try:
+            return int(s)
+        except ValueError:
+            try:
+                return float(s)
+            except ValueError:
+                return format_string(s)
+
+
+def format_string(in_string):
+    # Remove non-ascii characters
+    formatted = re.sub(r'[^\x00-\x7f]', r'', str(in_string))
+    formatted = ''.join(filter(lambda x: x in string.printable, formatted))
+    if len(formatted) == 1 and formatted == '?':
+        formatted = None
+    return formatted
+
+
+def get_seq_data(sequence, ignore_keys):
+    seq_dict = {}
+    for seq in sequence:
+        for s_key in seq.dir():
+            s_val = getattr(seq, s_key, '')
+            if type(s_val) is pydicom.uid.UID or s_key in ignore_keys:
+                continue
+
+            if type(s_val) == pydicom.sequence.Sequence:
+                _seq = get_seq_data(s_val, ignore_keys)
+                seq_dict[s_key] = _seq
+                continue
+
+            if type(s_val) == str:
+                s_val = format_string(s_val)
+            else:
+                s_val = assign_type(s_val)
+
+            if s_val:
+                seq_dict[s_key] = s_val
+    return seq_dict
+
+
+def get_csa_header(dcm):
+    exclude_tags = ['PhoenixZIP', 'SrMsgBuffer']
+    header = {}
+    try:
+        raw_csa_header = nibabel.nicom.dicomwrappers.SiemensWrapper(dcm).csa_header
+        tags = raw_csa_header['tags']
+    except:
+        log.warning('Failed to parse csa header!')
+        return header
+
+    for tag in tags:
+        if not raw_csa_header['tags'][tag]['items'] or tag in exclude_tags:
+            log.debug('Skipping : %s' % tag)
+            pass
+        else:
+            value = raw_csa_header['tags'][tag]['items']
+            if len(value) == 1:
+                value = value[0]
+                if type(value) == str and ( len(value) > 0 and len(value) < 1024 ):
+                    header[format_string(tag)] = format_string(value)
+                else:
+                    header[format_string(tag)] = assign_type(value)
+            else:
+                header[format_string(tag)] = assign_type(value)
+
+    return header
+
+
+def validate_timezone(zone):
+    # pylint: disable=missing-docstring
+    if zone is None:
+        zone = tzlocal.get_localzone()
+        if not zone:
+            zone = pytz.timezone('UTC')
+    else:
+        try:
+            zone = pytz.timezone(zone.zone)
+        except pytz.UnknownTimeZoneError:
+            zone = None
+    return zone
+
+
+def format_timestamp(date, time, timezone):
+    """
+    Return datetime formatted string
+    """
+    if date and time and timezone:
+        try:
+            return timezone.localize(datetime.datetime.strptime(date + time[:6], '%Y%m%d%H%M%S'), timezone)
+        except:
+            log.warning('Failed to create timestamp!')
+            log.info(date)
+            log.info(time)
+            log.info(timezone)
+            return None
+    return None
+
+
+def get_session_label(dcm):
+    """
+    Switch on manufacturer and either pull out the StudyID or the StudyInstanceUID
+    """
+    session_label = ''
+    if ( dcm.get('Manufacturer') and (dcm.get('Manufacturer').find('GE') != -1 or dcm.get('Manufacturer').find('Philips') != -1 ) and dcm.get('StudyID')):
+        session_label = dcm.get('StudyID')
+    else:
+        session_label = dcm.get('StudyInstanceUID')
+
+    return session_label
+
+
+def get_session_timestamp(dicom_ds, timezone):
+    if hasattr(dicom_ds, 'StudyDate') and hasattr(dicom_ds, 'StudyTime'):
+        study_date = dicom_ds.StudyDate
+        study_time = dicom_ds.StudyTime
+    elif hasattr(dicom_ds, 'StudyDateTime'):
+        study_date = dicom_ds.StudyDateTime[0:8]
+        study_time = dicom_ds.StudyDateTime[8:]
+    else:
+        study_date = None
+        study_time = None
+    session_timestamp = format_timestamp(study_date, study_time, timezone)
+    return session_timestamp
+
+
+def get_acquisition_timestamp(dicom_ds, timezone):
+    dicom_ds = dicom_acquisition_date_handler(dicom_ds)
+    if dicom_ds.get('AcquisitionDate') and dicom_ds.get('AcquisitionTime'):
+        acquisition_date = dicom_ds.AcquisitionDate
+        acquisition_time = dicom_ds.AcquisitionTime
+    elif getattr(dicom_ds, 'AcquisitionDateTime'):
+        acquisition_date = dicom_ds.AcquisitionDateTime[0:8]
+        acquisition_time = dicom_ds.AcquisitionDateTime[8:]
+    # The following allows the timestamps to be set for ScreenSaves
+    elif getattr(dicom_ds, 'ContentDate') and getattr(dicom_ds, 'ContentTime'):
+        acquisition_date = dicom_ds.ContentDate
+        acquisition_time = dicom_ds.ContentTime
+    else:
+        acquisition_date = None
+        acquisition_time = None
+
+    acquisition_timestamp = format_timestamp(acquisition_date, acquisition_time, timezone)
+    return acquisition_timestamp
+
+
+def parse_patient_age(age):
+    """
+    Parse patient age from string.
+    convert from 70d, 10w, 2m, 1y to datetime.timedelta object.
+    Returns age as duration in seconds.
+    """
+    if age == 'None' or not age:
+        return None
+
+    conversion = {  # conversion to days
+        'Y': 365.25,
+        'M': 30,
+        'W': 7,
+        'D': 1,
+    }
+    scale = age[-1:]
+    value = age[:-1]
+    if scale not in conversion.keys():
+        # Assume years
+        scale = 'Y'
+        value = age
+
+    age_in_seconds = datetime.timedelta(int(value) * conversion.get(scale)).total_seconds()
+
+    # Make sure that the age is reasonable
+    if not age_in_seconds or age_in_seconds <= 0:
+        age_in_seconds = None
+
+    return age_in_seconds
+
+
+def get_sex_string(sex_str):
+    """
+    Return male or female string.
+    """
+    if sex_str == 'M':
+        sex = 'male'
+    elif sex_str == 'F':
+        sex = 'female'
+    else:
+        sex = ''
+    return sex
+
+
+def dicom_acquisition_date_handler(dicom_ds):
+    if dicom_ds.get('AcquisitionDate'):
+        pass
+    elif dicom_ds.get('SeriesDate'):
+        dicom_ds.AcquisitionDate = dicom_ds.get('SeriesDate')
+    elif dicom_ds.get('StudyDate'):
+        dicom_ds.AcquisitionDate = dicom_ds.get('StudyDate')
+    else:
+        log.warning('No date found for DICOM file')
+    return dicom_ds
+
+
+def parse_file_dicom_metadata(dicom_ds):
+    # Extract the header values
+    header = {}
+    exclude_tags = ['[Unknown]', 'PixelData', 'Pixel Data',  '[User defined data]', '[Protocol Data Block (compressed)]', '[Histogram tables]', '[Unique image iden]']
+    tags = dicom_ds.dir()
+    for tag in tags:
+        try:
+            if (tag not in exclude_tags) and (type(dicom_ds.get(tag)) != pydicom.sequence.Sequence):
+                value = dicom_ds.get(tag)
+                if value or value == 0: # Some values are zero
+                    # Put the value in the header
+                    if type(value) == str and len(value) < 10240: # Max pydicom field length
+                        header[tag] = format_string(value)
+                    else:
+                        header[tag] = assign_type(value)
+                else:
+                    log.debug('No value found for tag: ' + tag)
+
+            if type(dicom_ds.get(tag)) == pydicom.sequence.Sequence:
+                seq_data = get_seq_data(dicom_ds.get(tag), exclude_tags)
+                # Check that the sequence is not empty
+                if seq_data:
+                    header[tag] = seq_data
+        except:
+            log.debug('Failed to get ' + tag)
+            pass
+    return header
+
+
+def parse_subject_metadata(dcm):
+    subject_dict = dict()
+    if hasattr(dcm, 'PatientSex') and get_sex_string(dcm.get('PatientSex')):
+        subject_dict['sex'] = get_sex_string(dcm.get('PatientSex'))
+    if hasattr(dcm, 'PatientAge') and dcm.get('PatientAge'):
+        try:
+            age = parse_patient_age(dcm.get('PatientAge'))
+            if age:
+                subject_dict['age'] = int(age)
+        except:
+            pass
+    if hasattr(dcm, 'PatientName') and dcm.get('PatientName').given_name:
+        # If the first name or last name field has a space-separated string, and one or the other field is not
+        # present, then we assume that the operator put both first and last names in that one field. We then
+        # parse that field to populate first and last name.
+        subject_dict['firstname'] = str(format_string(dcm.get('PatientName').given_name))
+        if not dcm.get('PatientName').family_name:
+            name = format_string(dcm.get('PatientName').given_name.split(' '))
+            if len(name) == 2:
+                first = name[0]
+                last = name[1]
+                subject_dict['lastname'] = str(last)
+                subject_dict['firstname'] = str(first)
+    if hasattr(dcm, 'PatientName') and dcm.get('PatientName').family_name:
+        subject_dict['lastname'] = str(format_string(dcm.get('PatientName').family_name))
+        if not dcm.get('PatientName').given_name:
+            name = format_string(dcm.get('PatientName').family_name.split(' '))
+            if len(name) == 2:
+                first = name[0]
+                last = name[1]
+                subject_dict['lastname'] = str(last)
+                subject_dict['firstname'] = str(first)
+    return subject_dict
+
+
+def parse_session_dicom_metadata(dicom_ds, timezone):
+    session_dict = dict()
+    session_timestamp = get_session_timestamp(dicom_ds, timezone)
+    session_label = get_session_label(dicom_ds)
+    if session_timestamp:
+        session_dict['timestamp'] = session_timestamp
+    if session_label:
+        session_dict['label'] = session_label
+    if hasattr(dicom_ds, 'OperatorsName'):
+        session_dict['operator'] = format_string(dicom_ds.get('OperatorsName'))
+    if hasattr(dicom_ds, 'PatientWeight') and dicom_ds.get('PatientWeight'):
+        session_dict['weight'] = assign_type(dicom_ds.get('PatientWeight'))
+    session_dict['subject'] = parse_subject_metadata(dicom_ds)
+    return session_dict
+
+
+def parse_acquisition_dicom_metadata(dicom_header_metadata):
+    acquisition_dict = dict()
+    if dicom_header_metadata.get('Modality'):
+        acquisition_dict['instrument'] = dicom_header_metadata.get('Modality')
+
+
+def get_pydicom_header(dcm):
+    # Extract the header values
+    header = {}
+    exclude_tags = ['[Unknown]', 'PixelData', 'Pixel Data',  '[User defined data]', '[Protocol Data Block (compressed)]', '[Histogram tables]', '[Unique image iden]']
+    tags = dcm.dir()
+    for tag in tags:
+        try:
+            if (tag not in exclude_tags) and ( type(dcm.get(tag)) != pydicom.sequence.Sequence ):
+                value = dcm.get(tag)
+                if value or value == 0: # Some values are zero
+                    # Put the value in the header
+                    if type(value) == str and len(value) < 10240: # Max pydicom field length
+                        header[tag] = format_string(value)
+                    else:
+                        header[tag] = assign_type(value)
+                else:
+                    log.debug('No value found for tag: ' + tag)
+
+            if type(dcm.get(tag)) == pydicom.sequence.Sequence:
+                seq_data = get_seq_data(dcm.get(tag), exclude_tags)
+                # Check that the sequence is not empty
+                if seq_data:
+                    header[tag] = seq_data
+        except:
+            log.debug('Failed to get ' + tag)
+            pass
+    return header
