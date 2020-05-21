66d7b6ff67894f285b6a9cbf2c1ac80da1816ccc
Nicolas Pannetier <nicolaspannetier@flywheel.io>
2020-05-21 08:42:19 -0700

    Fix backslash in VR string (#39)
    * Fix backslash in VR string

--- run.py
+++ run.py
@@ -29,6 +29,35 @@ log.setLevel('INFO')
 DEFAULT_TME = '120000.00'
 
 
+def fix_VM1_callback(dataset, data_element):
+    r"""Update the data element fixing VM based on public tag definition
+
+    This addresses the following none conformance for element with string VR having
+    a `\` in the their value which gets interpret as array by pydicom.
+    This function re-join string and is aimed to be used as callback.
+
+    From the DICOM Standard, Part 5, Section 6.2, for elements with a VR of LO, such as
+    Series Description: A character string that may be padded with leading and/or
+    spaces. The character code 5CH (the BACKSLASH "\" in ISO-IR 6) shall not be
+    present, as it is used as the delimiter between values in multi-valued data
+    elements. The string shall not have Control Characters except for ESC.
+
+    Args:
+        dataset (pydicom.DataSet): A pydicom DataSet
+        data_element (pydicom.DataElement): A pydicom DataElement from the DataSet
+
+    Returns:
+        pydicom.DataElement: An updated pydicom DataElement
+    """
+    vr, vm, _, _, _ = DicomDictionary.get(data_element.tag)
+    # Check if it is a VR string
+    if vr not in ['UT', 'ST', 'LT', 'FL', 'FD', 'AT', 'OB', 'OW', 'OF', 'SL', 'SQ',
+                  'SS', 'UL', 'OB/OW', 'OW/OB', 'OB or OW', 'OW or OB', 'UN'] \
+            and 'US' not in vr:
+        if vm == '1' and hasattr(data_element, 'VM') and data_element.VM > 1:
+            data_element._value = '\\'.join(data_element.value)
+
+
 def validate_dicom(path):
     with tempfile.TemporaryDirectory() as temp_dir:
         dicom_archive.DicomArchive(zip_path=path, extract_dir=temp_dir)
@@ -286,16 +315,30 @@ def get_seq_data(sequence, ignore_keys):
     return res
 
 
-def walk_dicom(dcm):
+def walk_dicom(dcm, callbacks=None, recursive=True):
+    """Same as pydicom.DataSet.walk but with logging the exception instead of raising.
+
+    Args:
+        dcm (pydicom.DataSet): A pydicom.DataSet.
+        callbacks (list): A list of function to apply on each DataElement of the
+            DataSet (default = None).
+        recursive (bool): It True, walk the dicom recursively when encountering a SQ.
+
+    Returns:
+        list: List of errors
+    """
     taglist = sorted(dcm._dict.keys())
     errors = []
     for tag in taglist:
         try:
             data_element = dcm[tag]
-            if tag in dcm and data_element.VR == "SQ":
+            if callbacks:
+                for cb in callbacks:
+                    cb(dcm, data_element)
+            if recursive and tag in dcm and data_element.VR == "SQ":
                 sequence = data_element.value
                 for dataset in sequence:
-                    walk_dicom(dataset)
+                    walk_dicom(dataset, callbacks, recursive=recursive)
         except Exception as ex:
             msg = f'With tag {tag} got exception: {str(ex)}'
             errors.append(msg)
@@ -323,7 +366,9 @@ def fix_type_based_on_dicom_vm(header):
 
 def get_pydicom_header(dcm):
     # Extract the header values
-    errors = walk_dicom(dcm)   # used to load all dcm tags in memory
+    # Load all dcm tags in memory and fix an issue found a LO VR with `\` in it (fix_VM1)
+    errors = walk_dicom(dcm, callbacks=[fix_VM1_callback], recursive=True)
+    # dcm.walk(fix_VM1, recursive=True)
     if errors:
         result = ''
         for error in errors:
@@ -397,9 +442,6 @@ def get_csa_header(dcm):
     return header
 
 
-
-
-
 def dicom_date_handler(dcm):
     if dcm.get('AcquisitionDate'):
         pass
@@ -419,7 +461,7 @@ def get_dcm_data_dict(dcm_path, force=False):
         'size': file_size,
         'force': force,
         'pydicom_exception': False,
-        'header': None
+        'header': {}
     }
     if file_size > 0:
         try:
