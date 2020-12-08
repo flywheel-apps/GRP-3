024adc410ce777caf92e972282a33a8efa302434
Nate Richman <naterichman@flywheel.io>
2020-12-08 15:12:45 -0600

    MAIN: Add test for dicom_tag_value_dict

--- tests/unit_tests/test_dicom_archive.py
+++ tests/unit_tests/test_dicom_archive.py
@@ -1,4 +1,5 @@
 import tempfile
+import numpy as np
 import os
 import pathlib
 import zipfile
@@ -7,6 +8,7 @@ import pytest
 from pydicom.data import get_testdata_files
 
 from utils.dicom.dicom_archive import DicomArchive
+from unittest.mock import MagicMock
 
 
 def test_dicom_archive_class_validate():
@@ -37,8 +39,32 @@ def test_dicom_archive_class_validate():
             assert DicomArchive(not_dicom_path, extract_dir=temp_dir, validate=True)
         assert 'failed to parse DICOMs' in str(err.value)
 
+
+im_arr = [[0.9989550114, 0.04570378363, 0, 0, 0, -1], [0.9989550114, 0.04570382088, 0, 0, 0, -1], [0.9989550114, 0.04570381716, 0, 0, 0, -1], [0.9989550114, 0.04570382088, 0, 0, 0, -1],[1, 0, 0, 0, 1, 0]]
+out = [[-0.0, 0.009, 0.0, 0.0, -0.2, -0.2], [-0.0, 0.009, 0.0, 0.0, -0.2, -0.2], [-0.0, 0.009, 0.0, 0.0, -0.2, -0.2], [-0.0, 0.009, 0.0, 0.0, -0.2, -0.2], [0.001, -0.037, 0.0, 0.0, 0.8, 0.8]]
 def test_contains_localizer_round():
-    im_arr = [[0.9989550114, 0.04570378363, 0, 0, 0, -1], [0.9989550114, 0.04570382088, 0, 0, 0, -1], [0.9989550114, 0.04570381716, 0, 0, 0, -1], [0.9989550114, 0.04570382088, 0, 0, 0, -1],[1, 0, 0, 0, 1, 0]]
-    out = [[-0.0, 0.009, 0.0, 0.0, -0.2, -0.2], [-0.0, 0.009, 0.0, 0.0, -0.2, -0.2], [-0.0, 0.009, 0.0, 0.0, -0.2, -0.2], [-0.0, 0.009, 0.0, 0.0, -0.2, -0.2], [0.001, -0.037, 0.0, 0.0, 0.8, 0.8]]
 
     assert DicomArchive._round_iop(im_arr).tolist() ==  out
+
+def test_dicom_tag_value_dict(mocker):
+    zip_mock = mocker.patch('utils.dicom.dicom_archive.zipfile')
+    mocker.patch('utils.dicom.dicom_archive.DicomArchive._validate')
+    mocker.patch('utils.dicom.dicom_archive.DicomArchive.initialize_dataset')
+    archive = DicomArchive('test','test2')
+
+
+    tag_vals = mocker.patch.object(archive, 'dicom_tag_value_list')
+    tag_vals.return_value = im_arr
+    ims = []
+    for im in im_arr:
+        im_patch = MagicMock()
+        im_patch.header_dict.get.return_value = im
+        ims.append(im_patch)
+    archive.dataset_list = ims
+
+    rounded = archive.dicom_tag_value_dict('ImageOrientationPatient')
+    assert list(rounded.keys()) == [tuple(o) for o in np.unique(np.array(out),axis=0).tolist()]
+
+
+#def test_create_dicom_tag_value_dict_IOP():
+
